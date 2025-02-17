238f40506d240513ea8d4de7a572f94a71a6ba6e
fcedolini <fcedolini@gmail.com>
2025-02-17 13:50:51 -0300

    Configurando ALLOWED_HOSTS

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-rqd9ryr%+735to4jyrx(oj+0e9r+iwv2k4785&@dwznmx$r&wz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["tu-app.herokuapp.com"]
+ALLOWED_HOSTS = ["lista-pendientes-8a3822cd752a.herokuapp.com",
+                 "127.0.0.1", "localhost"]
 
 
 # Application definition
